7f62e6a91c0fa5340f3b1455465a64ac69e08be6
rickyjulpiter <rickyjulpiter14@gmail.com>
2021-02-27 23:00:21 +0700

    Integrate firebase and create sign in component and sign in with Google Account

--- src/App.js
+++ src/App.js
@@ -5,17 +5,43 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/shop" component={ShopPage} />
-            </Switch>
-        </div>
-    );
+import { auth } from "./firebase/firebase.utils";
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentUser: null,
+        };
+    }
+
+    unsubsribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubsribeFromAuth = auth.onAuthStateChanged((user) => {
+            this.setState({ currentUser: user });
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubsribeFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header currentUser={this.state.currentUser} />
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/shop" component={ShopPage} />
+                    <Route path="/signin" component={SignInAndSignUpPage} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
