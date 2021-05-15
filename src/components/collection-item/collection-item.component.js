dde86a226eb663c04899fdeead62f245c220e1b6
Ricky J <rickyjulpiter14@gmail.com>
2021-05-15 18:46:22 +0700

    move directory and shop data into its own reducer, created respective selectors and updated corresponding component with redux flow for directory/shop

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -4,19 +4,22 @@ import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
 import { addItem } from "../../redux/cart/cart.actions";
 
-import "./collection.item.styles.scss";
+import "./collection-item.styles.scss";
 
 const CollectionItem = ({ item, addItem }) => {
     const { name, price, imageUrl } = item;
+
     return (
         <div className="collection-item">
             <div
                 className="image"
-                style={{ backgroundImage: `url(${imageUrl})` }}
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }}
             />
             <div className="collection-footer">
                 <span className="name">{name}</span>
-                <span className="price">{price} </span>
+                <span className="price">{price}</span>
             </div>
             <CustomButton onClick={() => addItem(item)} inverted>
                 Add to cart
